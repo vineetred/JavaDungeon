a9e2644b39154175631c2405df89bb16ad3dadce
Vineet Reddy <hotshot2797@gmail.com>
2021-10-31 13:16:19 -0400

    Some more documentation

--- src/model/DungeonImpl.java
+++ src/model/DungeonImpl.java
@@ -24,15 +24,20 @@ public class DungeonImpl implements Dungeon {
   private ArrayList<Edge> leftOverEdges;
   private ArrayList<Edge> finalEdges;
 
-  /**This creates a dungeon that requires the specification of whether the dungeon should wrap or not. How many rows and columns there should be specified as integers. The degree of interconnectivity(default is 0) or how many paths between nodes should there be. An interconnectivity of 0 means that there is exactly 1 path between all nodes. Each degree above that is an additional edge/connection added to the map. Finally, what percentage of caves should have treasure in it. The default is 20%. Caves are defined as having 1, 3, or 4 entrances. Tunnels only have 2 entrances and do not have treasure.
-          Params:
-  wraps – A boolean which determines if a dungeon wraps its edges around to the other side.
-  rows – The number of rows in the dungeon as an integer.
-  columns – The number of columns in the dungeon as an integer.
-  interconnect – The level of interconnectivity expressed as an integer. Default is 0.
-  treasure – The percentage of caves with treasure expressed as an integer. Default is 20.
-  Returns:
-  The dungeon built to specification represented as a 2 dimensional array.**/
+  /** The dungeon constructor.
+   * How many rows and columns there should be specified as integers.
+   * The degree of interconnectivity(default is 0) or how many paths between nodes should there be.
+   * An interconnectivity of 0 means that there is exactly 1 path between all nodes. Each degree above that is an
+   * additional edge/connection added to the map. Finally, what percentage of caves should have treasure in it.
+   * The default is 20%. Caves are defined as having 1, 3, or 4 entrances. Tunnels only have 2 entrances and do not
+   * have treasure.
+   * @param wraps boolean that is set true if you want it wrapped.
+   * @param rows the number of rows
+   * @param columns the number of columns
+   * @param interconnect the degree of interconnectivity you want the maze to be generated with
+   * @param treasure the percentage denominated out of 100 which
+   * forms the minimum threshold during treasure generation
+   */
   public DungeonImpl(boolean wraps, int rows, int columns, int interconnect, int treasure) {
     Cave[][] Gameboard = new Cave[rows][columns];
     this.wraps = wraps;
@@ -89,7 +94,7 @@ public class DungeonImpl implements Dungeon {
     }
 
     if (treasure < 20) {
-      throw new IllegalArgumentException("You must have at least 20% treasure.");
+      throw new IllegalArgumentException("You must have at least 20% treasure. This is not an acceptable threshold.");
     }
 
     if (interconnect < 0) {
